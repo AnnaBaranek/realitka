c5dd71ce1698b3f64c1eff7380173807e4957ecd
Anna Baranek <annaxbaranek@gmail.com>
2024-04-18 23:17:34 +0200

    oprava path bez html

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,10 +4,15 @@ import "./index.css";
 import { Header } from "../components/Header";
 import { Estate } from "../components/Estate";
 
+console.log('funguju')
 
 const currentPath = window.location.pathname;
+const pathWithoutHTML = currentPath.slice(0, -5)
+
+console.log(pathWithoutHTML)
+
 const response = await fetch(
-  `https://apps.kodim.cz/daweb/trening-api/apis/realitka/${currentPath}`
+  `https://apps.kodim.cz/daweb/trening-api/apis/realitka/${pathWithoutHTML}`
 );
 const data = await response.json();
 
@@ -21,19 +26,16 @@ console.log(data.contact.phone);
 console.log(data.photo);
 
 
-
-
-
 document.querySelector("#root").innerHTML = render(
   <div className="container">
     <Header
-      odkaz1="/dum01"
+      odkaz1="/dum01.html" 
       label1="Dům 1"
-      odkaz2="/dum02"
+      odkaz2="/dum02.html" 
       label2="Dům 2"
-      odkaz3="/dum03"
+      odkaz3="/dum03.html"
       label3="Dům 3"
-      odkaz4="/dum04"
+      odkaz4="/dum04.html"
       label4="Dům 4"
     />
   <Estate 
